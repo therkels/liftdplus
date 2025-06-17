d1a4d77178f7e4f1243b17304c5f9d4bdbb31a3b
therkels <jakob.therkelsen@gmail.com>
2025-06-17 17:50:56 -0400

    terraform configs

---
+++ infra/deployment/main.tf
@@ -0,0 +1,41 @@
+terraform {
+  required_providers {
+    vercel = {
+      source  = "vercel/vercel"
+      version = "~> 0.3"
+    }
+  }
+}
+
+provider "vercel" {
+  api_token = var.vercel_api_token
+}
+
+variable "vercel_api_token" {
+  description = "Vercel API Token"
+  type        = string
+  sensitive   = true
+}
+
+variable "project_id" {
+  description = "Vercel Project ID"
+  type        = string
+}
+
+data "vercel_project_directory" "web" {
+  path = "../../liftdplus-web"
+}
+
+resource "vercel_deployment" "dev" {
+  project_id  = var.project_id
+  files       = data.vercel_project_directory.web.files
+  path_prefix = "../../liftdplus-web"
+  production  = false
+}
+
+resource "vercel_deployment" "prod" {
+  project_id  = var.project_id
+  files       = data.vercel_project_directory.web.files
+  path_prefix = "../../liftdplus-web"
+  production  = true
+}
