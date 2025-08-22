e63d80e2b79fc8276fa623ad8cf47cbc50053db6
therkels <jakob.therkelsen@gmail.com>
2025-08-22 10:25:08 -0400

    get a single post

--- liftdplus-web/src/app/api/v0/posts/[[...posts]]/route.ts
+++ liftdplus-web/src/app/api/v0/posts/[[...posts]]/route.ts
@@ -6,6 +6,16 @@ export async function GET(
     { params }: {params: {posts:string}}
     ) {
     const supabase = await createClient();
+    const {
+        data: {user}
+    } = await supabase.auth.getUser();
+
+    if (!user) {
+        return new Response(JSON.stringify({error:"Not authenticated."}),{
+            status:401,
+            headers: {'Content-Type':"application/json"}
+        })
+    }
     const param_list = params.posts || [];
     const url = new URL(request.url);
     //case 1: Get all posts
@@ -24,11 +34,13 @@ export async function GET(
     }
     //get by post 
     else if (param_list.length === 1) {
-
+        const { data, error } = await supabase.rpc('get_post', {
+            post_id:param_list[0],
+            user_id:user.id,
+        });
+        return new Response(JSON.stringify({message: data}), {
+            status: 200,
+            headers: { "Content-Type": "application/json" }
+        })
     }
-
-    return new Response(JSON.stringify({message: params}), {
-        status: 200,
-        headers: { "Content-Type": "application/json" }
-    })
 }
