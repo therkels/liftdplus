e833809a5d48bf70de99a46336a187c7bc9b0151
Ealoulou <erin@liftdplus.com>
2025-10-10 10:54:28 -0400

    Update layout.tsx

--- liftdplus-web/src/app/layout.tsx
+++ liftdplus-web/src/app/layout.tsx
@@ -5,6 +5,8 @@ import ConditionalNavBar from "@/components/site_core/ConditionalNavBar";
 import ConditionalMain from "@/components/site_core/ConditionalMain";
 import { ToastProvider } from "@/contexts/ToastContext";
 import ToastContainer from "@/components/site_core/ToastContainer";
+import Script from "next/script";
+
 
 const inter = Inter({
   variable: "--font-inter",
@@ -28,13 +30,29 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${inter.variable} antialiased min-h-screen bg-gray-50`}>
-        <ToastProvider>
-          <ConditionalNavBar />
-          <ConditionalMain>{children}</ConditionalMain>
-          <ToastContainer />
-        </ToastProvider>
-      </body>
-    </html>
-  );
-}
+     return (
+  <html lang="en">
+    <head>
+      {/* Google Analytics */}
+      <Script
+        async
+        src="https://www.googletagmanager.com/gtag/js?id=G-NYZ84B03HS"
+      />
+      <Script id="google-analytics">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-NYZ84B03HS');
+        `}
+      </Script>
+    </head>
+
+    <body className={`${inter.variable} antialiased min-h-screen bg-gray-50`}>
+      <ConditionalNavBar />
+      <ConditionalMain>{children}</ConditionalMain>
+      <ToastContainer />
+    </body>
+  </html>
+);
+
