52c5be8700b54a61ef7a921db253ff872a35ad14
Ealoulou <erin@liftdplus.com>
2025-11-09 21:02:14 -0500

    Update UnregisterSW.tsx

--- liftdplus-web/src/components/dev/UnregisterSW.tsx
+++ liftdplus-web/src/components/dev/UnregisterSW.tsx
@@ -1,18 +1,31 @@
 "use client";
+
 import { useEffect } from "react";
 
 export default function UnregisterSW() {
   useEffect(() => {
-    // Unregister all service workers
-    if ("serviceWorker" in navigator) {
-      navigator.serviceWorker.getRegistrations?.().then((regs) => {
-        regs.forEach((r) => r.unregister());
-      });
-    }
-    // Clear any caches the SW created
-    if (typeof caches !== "undefined" && caches.keys) {
-      caches.keys().then((keys) => keys.forEach((k) => caches.delete(k)));
-    }
+    (async () => {
+      // Unregister all existing service workers
+      if ("serviceWorker" in navigator) {
+        try {
+          const regs = await navigator.serviceWorker.getRegistrations();
+          await Promise.all(regs.map((r) => r.unregister()));
+        } catch (_) {
+          // ignore errors
+        }
+      }
+
+      // Clear any caches they created
+      if (typeof caches !== "undefined") {
+        try {
+          const keys = await caches.keys();
+          await Promise.all(keys.map((k) => caches.delete(k)));
+        } catch (_) {
+          // ignore errors
+        }
+      }
+    })();
   }, []);
+
   return null;
 }
